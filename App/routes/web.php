b130b804162b2bc53b60cd297cca1ee94cb10560
DeliaGlez <deliavargas.glez@gmail.com>
2024-11-19 11:03:41 -0700

    A7

--- App/routes/web.php
+++ App/routes/web.php
@@ -6,6 +6,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/operacion/suma/{n1}/{n2}', function ($n1= 0,$n2=0) {
+    $suma= $n1+$n2;
+    return $suma;
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+']);
+Route::get('/operacion/resta/{n1}/{n2}', function ($n1= 0,$n2=0) {
+    $suma= $n1-$n2;
+    return $suma;
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+']);
+Route::get('/operacion/multiplicacion/{n1}/{n2}', function ($n1= 0,$n2=0) {
+    $suma= $n1*$n2;
+    return $suma;
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+']);
+Route::get('/operacion/division/{n1}/{n2}', function ($n1= 0,$n2=0) {
+    $suma= $n1/$n2;
+    return $suma;
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+']);
+
+Route::get('/saludar/{nombre}/{apellido?}', function ($name,$apellido= "Apellido") {
+    return 'Hola '. $name.' '. $apellido;
+})->where(['nombre' => '[a-zA-z]+', 'apellido' => '[a-z]+']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   
 });
 
 require __DIR__.'/auth.php';
